baad302898331262c40274694a99d8c9f675c10b
uniaue <115631356+unique571@users.noreply.github.com>
2023-04-17 07:39:47 +0100

    ready to work

---
+++ src/utils/Data.js
@@ -0,0 +1,28 @@
+ export const services =[
+   {
+    title: "Free Shipping",
+    tagline: "From all orders over $5",
+    image: "images/service.png",
+   },
+   {
+    title: "Daily Surprise Offers ",
+    tagline: "Save upto 25%  off",
+    image: "images/service-02.png",
+   },
+   {
+    title: "Support 24/7",
+    tagline: "Shop with experts",
+    image: "images/service-03.png",
+   },
+   {
+    title: "Affordable prices ",
+    tagline: "Get Factory Default Price",
+    image: "images/service-04.png",
+   },
+   {
+    title: "Secure payment ",
+    tagline: "100% Protected Payment",
+    image: "images/service-05.png",
+   },
+
+];
